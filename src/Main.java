aa5874de3464f45b1136a6d3ebfc3f10e23868e8
ironboy <thomas@nodehill.com>
2022-09-22 15:15:16 +0200

    Changed Hello:s to Hi:s

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Manuel!");
-        System.out.println("Hello Thomas!");
+        System.out.println("Hi Manuel!");
+        System.out.println("Hi Thomas!");
     }
 }
