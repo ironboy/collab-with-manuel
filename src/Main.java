335bcf1606421a6c0159001ec58fb812c63c9992
ManneWas <mannewas@protonmail.com>
2022-09-22 15:27:06 +0200

    added greeting message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Greetings Manuel!");
+        System.out.println("Greetings Thomas!");
         System.out.println("Bye Manuel!");
         System.out.println("Bye Thomas!");
         System.out.println("Hi Manuel!");
